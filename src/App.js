a75605bc2e626410bcf9450f9cca1c14c11488cb
Abhishek Kapil <kapil_abhishek@rocketmail.com>
2020-05-14 15:26:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Heading from "./Heading.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: "",
+      list: []
+
+    }
+    this.addItem = this.addItem.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    })
+    console.log(this.state.input);
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    this.state.input !== "" && this.setState({
+      list: [...this.state.list, this.state.input],
+      input: ""
+    })
+  }
+  
+  removeItem(item){
+    let newList = this.state.list.filter(otherItems => otherItems !== item)
+    this.setState({
+      list: [...newList]
+    })
+  }
+
+
+
+  render() {
+    const { list,input } = this.state;
+    return (
+      <div className="App">
+        <Heading />
+        <form onSubmit = {this.addItem}>
+          <input type ="text" value ={input} onChange ={this.handleChange} placeholder= "Please type item name here..." style = {{width: 450}}/>
+          <div>
+            <button type="submit">Add to Shopping List</button>
+          </div>
+          <ul>
+            {list.map((item,index) => 
+            <div>
+              <li key = {index}>{item}{' '}
+              <button onClick = {(e) =>this.removeItem(item)}>
+              delete
+              </button>
+                </li>
+                </div>
+            )}
+          </ul>            
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Heading() {
+    return (
+        <div>
+            <h1>My Shopping List</h1>
+            <h3>By: Abhishek Kapil</h3>
+            <h4>Please enter your items in the list:</h4>
+        </div>
+    )
+};
+
+
+
+export default Heading;
